e49867ff9fd5dffcb2a039621f40c51ca5dfce50
Dan Schulte <daschult@microsoft.com>
2018-04-16 16:28:32 -0700

    Add logging to enumSpec

--- lib/serialization/enumSpec.ts
+++ lib/serialization/enumSpec.ts
@@ -1,7 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
-import { TypeSpec, createValidationErrorMessage } from "./typeSpec";
+import { TypeSpec, createValidationErrorMessage, createValidationWarningMessage } from "./typeSpec";
 import { PropertyPath } from "./propertyPath";
+import { SerializationOptions, log } from "./serializationOptions";
+import { HttpPipelineLogLevel } from "../httpPipelineLogLevel";
 
 export interface EnumTypeSpec<T> extends TypeSpec<T, T> {
   /**
@@ -26,22 +28,34 @@ export function enumSpec<T>(enumName: string, allowedValues: T[]): EnumTypeSpec<
 
     allowedValues: allowedValues,
 
-    serialize(propertyPath: PropertyPath, value: T): T {
+    serialize(propertyPath: PropertyPath, value: T, options: SerializationOptions): T {
       const foundMatch: boolean = allowedValues.some((item) => {
         return item === value || (typeof item === "string" && typeof value === "string" && item.toLowerCase() === value.toLowerCase());
       });
       if (!foundMatch) {
-        throw new Error(createValidationErrorMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`));
+        if (options && options.serializationStrictTypeChecking) {
+          const errorMessage: string = createValidationErrorMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`);
+          log(options, HttpPipelineLogLevel.ERROR, errorMessage);
+          throw new Error(errorMessage);
+        } else {
+          log(options, HttpPipelineLogLevel.WARNING, createValidationWarningMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`));
+        }
       }
       return value;
     },
 
-    deserialize(propertyPath: PropertyPath, value: T): T {
+    deserialize(propertyPath: PropertyPath, value: T, options: SerializationOptions): T {
       const foundMatch: boolean = allowedValues.some((item) => {
         return item === value || (typeof item === "string" && typeof value === "string" && item.toLowerCase() === value.toLowerCase());
       });
       if (!foundMatch) {
-        throw new Error(createValidationErrorMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`));
+        if (options && options.deserializationStrictTypeChecking) {
+          const errorMessage: string = createValidationErrorMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`);
+          log(options, HttpPipelineLogLevel.ERROR, errorMessage);
+          throw new Error(errorMessage);
+        } else {
+          log(options, HttpPipelineLogLevel.WARNING, createValidationWarningMessage(propertyPath, value, `one of the enum allowed values: ${JSON.stringify(allowedValues)}`));
+        }
       }
       return value;
     }
--- test/serialization/enumSpecTests.ts
+++ test/serialization/enumSpecTests.ts
@@ -2,40 +2,43 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 import * as assert from "assert";
 import { enumSpec } from "../../lib/serialization/enumSpec";
-import { serializeTest } from "./specTest";
+import { serializeTest, deserializeTest } from "./specTest";
 
 describe("enumSpec", () => {
   it("should have \"Enum\" for its specType property", () => {
     assert.strictEqual("Enum", enumSpec("Letters", []).specType);
   });
 
-  it("shoudl have the correct enumName property", () => {
+  it("should have the correct enumName property", () => {
     assert.strictEqual("Letters", enumSpec("Letters", []).enumName);
   });
 
   describe("serialize()", () => {
     describe("with strict type-checking", () => {
-      function enumSerializeWithStrictTypeCheckingTest<T>(args: { allowedValues: T[], value: T, expectedResult: T | Error }): void {
+      function enumSerializeWithStrictTypeCheckingTest<T>(args: { allowedValues: T[], value: T, expectedResult: T | Error, expectedLogs?: string[] }): void {
         serializeTest({
           typeSpec: enumSpec("Letters", args.allowedValues),
           options: {
             serializationStrictTypeChecking: true
           },
           value: args.value,
-          expectedResult: args.expectedResult
+          expectedResult: args.expectedResult,
+          expectedLogs: args.expectedLogs
         });
       }
 
       enumSerializeWithStrictTypeCheckingTest({
         allowedValues: ["a", "b", "c"],
-        value: <any>undefined,
-        expectedResult: new Error(`Property a.property.path with value undefined must be one of the enum allowed values: ["a","b","c"].`)
+        value: undefined as any,
+        expectedResult: new Error(`Property a.property.path with value undefined must be one of the enum allowed values: ["a","b","c"].`),
+        expectedLogs: [`ERROR: Property a.property.path with value undefined must be one of the enum allowed values: ["a","b","c"].`]
       });
 
       enumSerializeWithStrictTypeCheckingTest({
         allowedValues: ["a", "b", "c"],
-        value: "",
-        expectedResult: new Error(`Property a.property.path with value "" must be one of the enum allowed values: ["a","b","c"].`)
+        value: "" as any,
+        expectedResult: new Error(`Property a.property.path with value "" must be one of the enum allowed values: ["a","b","c"].`),
+        expectedLogs: [`ERROR: Property a.property.path with value "" must be one of the enum allowed values: ["a","b","c"].`]
       });
 
       enumSerializeWithStrictTypeCheckingTest({
@@ -45,6 +48,128 @@ describe("enumSpec", () => {
       });
 
       enumSerializeWithStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "A",
+        expectedResult: "A"
+      });
+    });
+
+    describe("without strict type-checking", () => {
+      function enumSerializeWithoutStrictTypeCheckingTest<T>(args: { allowedValues: T[], value: T, expectedResult: T | Error, expectedLogs?: string[] }): void {
+        serializeTest({
+          typeSpec: enumSpec("Letters", args.allowedValues),
+          options: {
+            serializationStrictTypeChecking: false
+          },
+          value: args.value,
+          expectedResult: args.expectedResult,
+          expectedLogs: args.expectedLogs
+        });
+      }
+
+      enumSerializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: undefined as any,
+        expectedResult: undefined as any,
+        expectedLogs: [`WARNING: Property a.property.path with value undefined should be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumSerializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "" as any,
+        expectedResult: "" as any,
+        expectedLogs: [`WARNING: Property a.property.path with value "" should be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumSerializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "a",
+        expectedResult: "a"
+      });
+
+      enumSerializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "A",
+        expectedResult: "A"
+      });
+    });
+  });
+
+  describe("deserialize()", () => {
+    describe("with strict type-checking", () => {
+      function enumDeserializeWithStrictTypeCheckingTest<T>(args: { allowedValues: T[], value: T, expectedResult: T | Error, expectedLogs?: string[] }): void {
+        deserializeTest({
+          typeSpec: enumSpec("Letters", args.allowedValues),
+          options: {
+            deserializationStrictTypeChecking: true
+          },
+          value: args.value,
+          expectedResult: args.expectedResult,
+          expectedLogs: args.expectedLogs
+        });
+      }
+
+      enumDeserializeWithStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: undefined as any,
+        expectedResult: new Error(`Property a.property.path with value undefined must be one of the enum allowed values: ["a","b","c"].`),
+        expectedLogs: [`ERROR: Property a.property.path with value undefined must be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumDeserializeWithStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "" as any,
+        expectedResult: new Error(`Property a.property.path with value "" must be one of the enum allowed values: ["a","b","c"].`),
+        expectedLogs: [`ERROR: Property a.property.path with value "" must be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumDeserializeWithStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "a",
+        expectedResult: "a"
+      });
+
+      enumDeserializeWithStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "A",
+        expectedResult: "A"
+      });
+    });
+
+    describe("without strict type-checking", () => {
+      function enumDeserializeWithoutStrictTypeCheckingTest<T>(args: { allowedValues: T[], value: T, expectedResult: T | Error, expectedLogs?: string[] }): void {
+        deserializeTest({
+          typeSpec: enumSpec("Letters", args.allowedValues),
+          options: {
+            deserializationStrictTypeChecking: false
+          },
+          value: args.value,
+          expectedResult: args.expectedResult,
+          expectedLogs: args.expectedLogs
+        });
+      }
+
+      enumDeserializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: undefined as any,
+        expectedResult: undefined as any,
+        expectedLogs: [`WARNING: Property a.property.path with value undefined should be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumDeserializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "" as any,
+        expectedResult: "" as any,
+        expectedLogs: [`WARNING: Property a.property.path with value "" should be one of the enum allowed values: ["a","b","c"].`]
+      });
+
+      enumDeserializeWithoutStrictTypeCheckingTest({
+        allowedValues: ["a", "b", "c"],
+        value: "a",
+        expectedResult: "a"
+      });
+
+      enumDeserializeWithoutStrictTypeCheckingTest({
         allowedValues: ["a", "b", "c"],
         value: "A",
         expectedResult: "A"
