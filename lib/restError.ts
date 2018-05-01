0b95b7b494e6075303a390e8dacaaba5a45b8cc6
Dan Schulte <daschult@microsoft.com>
2018-05-01 15:34:59 -0700

    Strip authorization header off of the RestError response's request property too

--- lib/restError.ts
+++ lib/restError.ts
@@ -3,6 +3,15 @@
 
 import { HttpRequest, HttpResponse } from "./msRest";
 
+function stripAuthorizationHeader(httpRequest: HttpRequest): HttpRequest {
+  let result: HttpRequest = httpRequest;
+  if (result.headers.contains("authorization")) {
+    result = result.clone();
+    result.headers.remove("authorization");
+  }
+  return result;
+}
+
 export interface RestErrorProperties {
   code?: string;
   statusCode?: number;
@@ -23,12 +32,13 @@ export class RestError extends Error implements RestErrorProperties {
     if (properties) {
       this.code = properties.code;
       this.statusCode = properties.statusCode;
-      this.request = properties.request;
-      if (this.request && this.request.headers.contains("authorization")) {
-        this.request = this.request.clone();
-        this.request.headers.remove("authorization");
+      if (properties.request) {
+        this.request = stripAuthorizationHeader(properties.request);
       }
       this.response = properties.response;
+      if (this.response && this.response.request) {
+        this.response.request = stripAuthorizationHeader(this.response.request);
+      }
       this.body = properties.body;
     }
   }
