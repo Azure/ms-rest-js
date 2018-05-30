1462757ba20a180a87a4d63ed06725a28618ee06
Rikki Gibson <rigibson@microsoft.com>
2018-05-30 13:30:35 -0700

    Run node tests in script

--- .scripts/chrome-unit.js
+++ .scripts/chrome-unit.js
@@ -1,33 +1,64 @@
-const semver = require("semver");
-if (semver.major(process.version) < 8) {
-  // Ignore browser tests in pre-node 8 CI job
-  console.log("node.js " + process.version + " too low. Exiting.");
-  process.exit(0);
-}
-
+const { major } = require("semver");
 const { spawn, exec } = require("child_process");
 const { join } = require("path");
 
 const webpackDevServer = spawn(join(__dirname, "../node_modules/.bin/ts-node"), [join(__dirname, "../testserver")], { shell: true })
+function cleanupDevServer() {
+  webpackDevServer.stderr.destroy();
+  webpackDevServer.stdout.destroy();
+  webpackDevServer.kill();
+};
 
-let mochaChromeRunning = false
+let mochaRunning = false
 const webpackDevServerHandler = (data) => {
-  if (!mochaChromeRunning) {
-    mochaChromeRunning = true
-    const mochaChrome = spawn(
-      join(__dirname, "../node_modules/.bin/mocha-chrome"),
-      ["http://localhost:3001", JSON.stringify(['"--no-sandbox"'])],
-      { shell: true, stdio: "inherit" });
-    mochaChrome.on("exit", (status) => {
-      webpackDevServer.stderr.destroy();
-      webpackDevServer.stdout.destroy();
-      webpackDevServer.kill();
+  if (!mochaRunning) {
+    mochaRunning = true
 
-      process.exit(status);
+    const mochaChromePromise = new Promise((resolve, reject) => {
+      if (major(process.version) < 8) {
+        // Skip browser tests in pre-node 8
+        resolve();
+      } else {
+        const mochaChromeExecutable = join(__dirname, "../node_modules/.bin/mocha-chrome");
+        exec(`${mochaChromeExecutable} http://localhost:3001`, (err, stdout, stderr) => {
+          if (err) {
+            reject(err);
+          } else {
+            console.log(stdout);
+            console.error(stderr);
+            resolve();
+          }
+        });
+      }
+    });
+
+    const mochaPromise = new Promise((resolve, reject) => {
+      const mochaExecutable = join(__dirname, "../node_modules/.bin/_mocha");
+      const mocha = exec(mochaExecutable, (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(stdout);
+          console.error(stderr);
+          resolve();
+        }
+      });
+    });
+
+    Promise.all([mochaPromise, mochaChromePromise]).then(res => {
+      cleanupDevServer();
+      process.exit(0);
+    }).catch((err) => {
+      console.error(err);
+      cleanupDevServer();
+      process.exit(1);
     });
   }
 }
 
-webpackDevServer.stderr.on('data', data => console.error(data.toString()));
+webpackDevServer.stderr.on('data', data => {
+  console.error("webpack dev server error:", data.toString());
+});
+
 webpackDevServer.stdout.on('data', webpackDevServerHandler);
 webpackDevServer.on('exit', webpackDevServerHandler);
