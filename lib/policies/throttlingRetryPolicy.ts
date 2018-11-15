02970eb9fabc927e9150f75dc3aa547b6d5dc452
Kamil Pajdzik <kamip@microsoft.com>
2018-11-15 12:40:28 -0800

    Add empty class and tests

---
+++ lib/policies/throttlingRetryPolicy.ts
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+import { BaseRequestPolicy, RequestPolicy, RequestPolicyOptions, RequestPolicyFactory } from "./requestPolicy";
+import { WebResource } from "../webResource";
+import { HttpOperationResponse } from "../httpOperationResponse";
+
+export function throttlingRetryPolicy(): RequestPolicyFactory {
+  return {
+    create: (nextPolicy: RequestPolicy, options: RequestPolicyOptions) => {
+      return new ThrottlingRetryPolicy(nextPolicy, options);
+    }
+  };
+}
+
+export class ThrottlingRetryPolicy extends BaseRequestPolicy {
+  constructor(nextPolicy: RequestPolicy, options: RequestPolicyOptions) {
+    super(nextPolicy, options);
+  }
+
+  public sendRequest(webResource: WebResource): Promise<HttpOperationResponse> {
+    throw new Error("Method not implemented.");
+  }
+}
