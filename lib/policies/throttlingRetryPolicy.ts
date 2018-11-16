03ebb001c715a0e0f35649596cfb58d00116f1e9
Kamil Pajdzik <kamip@microsoft.com>
2018-11-16 14:14:08 -0800

    Add basic unit tests

--- lib/policies/throttlingRetryPolicy.ts
+++ lib/policies/throttlingRetryPolicy.ts
@@ -4,6 +4,10 @@
 import { BaseRequestPolicy, RequestPolicy, RequestPolicyOptions, RequestPolicyFactory } from "./requestPolicy";
 import { WebResource } from "../webResource";
 import { HttpOperationResponse } from "../httpOperationResponse";
+import { Constants } from "../util/constants";
+
+type ResponseHandler = (response: HttpOperationResponse) => Promise<HttpOperationResponse>;
+const StatusCodes = Constants.HttpConstants.StatusCodes;
 
 export function throttlingRetryPolicy(): RequestPolicyFactory {
   return {
@@ -14,15 +18,24 @@ export function throttlingRetryPolicy(): RequestPolicyFactory {
 }
 
 export class ThrottlingRetryPolicy extends BaseRequestPolicy {
-  constructor(nextPolicy: RequestPolicy, options: RequestPolicyOptions) {
+  private _handleResponse: ResponseHandler;
+
+  constructor(nextPolicy: RequestPolicy, options: RequestPolicyOptions, _handleResponse?: ResponseHandler) {
     super(nextPolicy, options);
+    this._handleResponse = _handleResponse || this._defaultResponseHandler;
   }
 
   public sendRequest(httpRequest: WebResource): Promise<HttpOperationResponse> {
-    return this._nextPolicy.sendRequest(httpRequest).then(this._handleResponse);
+    return this._nextPolicy.sendRequest(httpRequest).then(response => {
+      if (response.status !== StatusCodes.TooManyRequests) {
+        return Promise.resolve(response);
+      } else {
+        return this._handleResponse(response);
+      }
+    });
   }
 
-  private _handleResponse(reponse: HttpOperationResponse) {
-
+  private _defaultResponseHandler(response: HttpOperationResponse): Promise<HttpOperationResponse> {
+    return Promise.reject();
   }
 }
--- test/shared/policies/throttlingRetryPolicyTests.ts
+++ test/shared/policies/throttlingRetryPolicyTests.ts
@@ -1,8 +1,71 @@
+import assert from "assert";
+import { ThrottlingRetryPolicy } from "../../../lib/policies/throttlingRetryPolicy";
+import { WebResource } from "../../../lib/webResource";
+import { HttpOperationResponse } from "../../../lib/httpOperationResponse";
+import { HttpHeaders, RequestPolicyOptions } from "../../../lib/msRest";
+
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-describe("ThrottlingRetryPolicy", () => {
-    describe("constructor", () => {
+describe.only("ThrottlingRetryPolicy", () => {
+    class PassThroughPolicy {
+        constructor(private _response: HttpOperationResponse) { }
+        public sendRequest(request: WebResource): Promise<HttpOperationResponse> {
+            const response = {
+                ...this._response,
+                request: request
+            };
 
+            return Promise.resolve(response);
+        }
+    }
+
+    function createDefaultThrottlingRetryPolicy(response?: HttpOperationResponse, actionHandler?: (response: HttpOperationResponse) => Promise<HttpOperationResponse>) {
+        if (!response) {
+            response = {
+                status: 200,
+                request: new WebResource(),
+                headers: new HttpHeaders()
+            };
+        }
+
+        const passThroughPolicy = new PassThroughPolicy(response);
+        return new ThrottlingRetryPolicy(passThroughPolicy, new RequestPolicyOptions(), actionHandler);
+    }
+
+    describe("sendRequest", () => {
+        it("does not modify the request", async () => {
+            const request = new WebResource();
+            request.url = "http://url";
+            request.method = "PATCH";
+            request.body = { someProperty: "someValue" };
+            request.headers = new HttpHeaders({ "header": "abc" });
+            request.query = { q: "param" };
+
+            const policy = createDefaultThrottlingRetryPolicy();
+            const response = await policy.sendRequest(request);
+
+            assert.deepEqual(response.request, request);
+        });
+
+        it("does nothing when status code is not 429", async () => {
+            const request = new WebResource();
+            const mockResponse = {
+                status: 400,
+                headers: new HttpHeaders({
+                    "Retry-After": "100"
+                }),
+                request: request
+            };
+            const policy = createDefaultThrottlingRetryPolicy(mockResponse, _ => assert.fail());
+
+            const response = await policy.sendRequest(request);
+
+            assert.deepEqual(response, mockResponse);
+        });
+
+        it("", async () => {
+
+        });
     });
 });
