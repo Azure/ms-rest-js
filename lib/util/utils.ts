9f7f508695e489ddb1c9daa0ab721f45a9754355
Kamil Pajdzik <kamip@microsoft.com>
2018-12-10 20:43:49 -0800

    Fix uuid import

--- lib/util/utils.ts
+++ lib/util/utils.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import * as uuid from "uuid";
+import uuidv4 from "uuid/v4";
 import { HttpOperationResponse } from "../httpOperationResponse";
 import { RestError } from "../restError";
 import { WebResource } from "../webResource";
@@ -110,7 +110,7 @@ export function objectValues(obj: { [key: string]: any; }): any[] {
  * @return {string} RFC4122 v4 UUID.
  */
 export function generateUuid(): string {
-  return uuid.v4();
+  return uuidv4();
 }
 
 /**
