5ef22b65fdc071a88867f704f8a6edca360c0b8e
Kamil Pajdzik <kamip@microsoft.com>
2019-05-08 10:20:53 -0700

    Add script to run tests on dependent projects

---
+++ .scripts/testDependentProjects.ts
@@ -0,0 +1,28 @@
+import { ExecOptions, execSync } from "child_process";
+import path from "path";
+
+function execAndLog(command: string, options?: ExecOptions) {
+  console.log(`\n\nExecuting "${command}"`);
+  execSync(command, {
+    ...options,
+    stdio: "inherit"
+  });
+}
+
+const msRestJsDirectory = path.join(__dirname, "..");
+
+console.log(process.argv);
+
+const projectName = process.argv[2];
+const projectDirectory = `.tmp/${projectName}`;
+const gitHubUrl = `https://github.com/Azure/${projectName}.git ${projectDirectory}`;
+
+execAndLog(`git clone ${gitHubUrl} --recursive`);
+execAndLog(`npm install`, { cwd: projectDirectory });
+execAndLog(`npm install ${msRestJsDirectory}`, { cwd: projectDirectory });
+
+const additionalCommands: string[] = process.argv.slice(3);
+additionalCommands.forEach(command => execAndLog(command, { cwd: projectDirectory }));
+
+execAndLog(`npm run test`, { cwd: projectDirectory });
+execAndLog(`rm -rf ${projectDirectory}`);
