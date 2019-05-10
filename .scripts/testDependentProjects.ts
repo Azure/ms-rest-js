d0a319dc3d2e7b2c9fbc6bcfcacbbf7052597a40
Kamil Pajdzik <kamip@microsoft.com>
2019-05-10 09:20:15 -0700

    Add git checkout

--- .scripts/testDependentProjects.ts
+++ .scripts/testDependentProjects.ts
@@ -37,6 +37,7 @@ async function execAndLog(executable: string, args?: string[], options?: RunOpti
     const gitHubUrl = `https://github.com/Azure/${projectName}.git`;
 
     await execAndLog(`git`, ["clone", gitHubUrl, projectDirectory, "--recursive"]);
+    await execAndLog(`git`, ["checkout", "kamip/test"]);
     await execAndLog(`npm`, [ "install", "../.." ], { executionFolderPath: projectDirectory });
     await execAndLog(`npm`, [ "install" ], { executionFolderPath: projectDirectory });
 
