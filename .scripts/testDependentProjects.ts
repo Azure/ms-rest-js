331f501987a1a1ff0943e0ce56dbda1454c77a45
Kamil Pajdzik <kamip@microsoft.com>
2019-05-09 20:28:11 -0700

    Change package name to ../..

--- .scripts/testDependentProjects.ts
+++ .scripts/testDependentProjects.ts
@@ -37,8 +37,8 @@ async function execAndLog(executable: string, args?: string[], options?: RunOpti
     const gitHubUrl = `https://github.com/Azure/${projectName}.git`;
 
     await execAndLog(`git`, ["clone", gitHubUrl, projectDirectory, "--recursive"]);
-    await execAndLog(`npm`, [ "install", "@azure/ms-rest-js@latest" ], { executionFolderPath: projectDirectory });
     await execAndLog(`npm`, [ "install", "../.." ], { executionFolderPath: projectDirectory });
+    await execAndLog(`npm`, [ "install" ], { executionFolderPath: projectDirectory });
 
     const additionalCommands: string[] = process.argv.slice(3);
     for (const command of additionalCommands) {
