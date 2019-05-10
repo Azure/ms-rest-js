34cb409d7694dbe46ee8ed84ffd85a0b1fb7359f
Kamil Pajdzik <kamip@microsoft.com>
2019-05-10 09:40:14 -0700

    Remove git branch checkout

--- .scripts/testDependentProjects.ts
+++ .scripts/testDependentProjects.ts
@@ -37,7 +37,6 @@ async function execAndLog(executable: string, args?: string[], options?: RunOpti
     const gitHubUrl = `https://github.com/Azure/${projectName}.git`;
 
     await execAndLog(`git`, ["clone", gitHubUrl, projectDirectory, "--recursive"]);
-    await execAndLog(`git`, ["checkout", "kamip/test"], { executionFolderPath: projectDirectory });
     await execAndLog(`npm`, [ "install", "../.." ], { executionFolderPath: projectDirectory });
     await execAndLog(`npm`, [ "install" ], { executionFolderPath: projectDirectory });
 
