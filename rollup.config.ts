37ac8b1508fe5a636ec9d3c5a8e418fc35f49873
Kamil Pajdzik <kamip@microsoft.com>
2019-05-20 15:30:24 -0700

    Add Fetch to browser

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,14 +5,14 @@
  /// <reference path=".typings/rollup-plugin-sourcemaps.d.ts" />
  /// <reference path=".typings/rollup-plugin-visualizer.d.ts" />
 
- import alias from "rollup-plugin-alias";
- import commonjs from "rollup-plugin-commonjs";
- import json from "rollup-plugin-json";
- import nodeResolve from "rollup-plugin-node-resolve";
- import sourcemaps from "rollup-plugin-sourcemaps";
- import visualizer from "rollup-plugin-visualizer";
+import alias from "rollup-plugin-alias";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
+import nodeResolve from "rollup-plugin-node-resolve";
+import sourcemaps from "rollup-plugin-sourcemaps";
+import visualizer from "rollup-plugin-visualizer";
 
- const banner = `/** @license ms-rest-js
+const banner = `/** @license ms-rest-js
   * Copyright (c) Microsoft Corporation. All rights reserved.
   * Licensed under the MIT License. See License.txt and ThirdPartyNotices.txt in the project root for license information.
   */`;
@@ -20,72 +20,72 @@
  /**
   * @type {import('rollup').RollupFileOptions}
   */
- const nodeConfig = {
-   input: "./es/lib/msRest.js",
-   external: [
-     "axios",
-     "form-data",
-     "node-fetch",
-     "os",
-     "stream",
-     "tough-cookie",
-     "tslib",
-     "tunnel",
-     "uuid/v4",
-     "xml2js",
-   ],
-   output: {
-     file: "./dist/msRest.node.js",
-     format: "cjs",
-     sourcemap: true,
-     banner
-   },
-   plugins: [
-     nodeResolve({
-       module: true
-     }),
-     commonjs(),
-     sourcemaps(),
-     json(),
-     visualizer({
-       filename: "dist/node-stats.html",
-       sourcemap: true
-     })
-   ]
- };
+const nodeConfig = {
+  input: "./es/lib/msRest.js",
+  external: [
+    "axios",
+    "form-data",
+    "node-fetch",
+    "os",
+    "stream",
+    "tough-cookie",
+    "tslib",
+    "tunnel",
+    "uuid/v4",
+    "xml2js",
+  ],
+  output: {
+    file: "./dist/msRest.node.js",
+    format: "cjs",
+    sourcemap: true,
+    banner
+  },
+  plugins: [
+    nodeResolve({
+      module: true
+    }),
+    commonjs(),
+    sourcemaps(),
+    json(),
+    visualizer({
+      filename: "dist/node-stats.html",
+      sourcemap: true
+    })
+  ]
+};
 
  /**
   * @type {import('rollup').RollupFileOptions}
   */
- const browserConfig = {
-   input: "./es/lib/msRest.js",
-   external: [],
-   output: {
-     file: "./dist/msRest.browser.js",
-     format: "umd",
-     name: "msRest",
-     sourcemap: true,
-     banner
-   },
-   plugins: [
-     alias({
-       "./defaultHttpClient": "./defaultHttpClient.browser",
-       "./policies/msRestUserAgentPolicy": "./policies/msRestUserAgentPolicy.browser",
-       "./policies/proxyPolicy": "./policies/proxyPolicy.browser",
-       "./util/xml": "./util/xml.browser",
-       "./util/base64": "./util/base64.browser",
-     }),
-     nodeResolve({
-       module: true,
-       browser: true
-     }),
-     commonjs(),
-     sourcemaps(),
-     visualizer({
-       filename: "dist/browser-stats.html",
-       sourcemap: true
-     })
-   ]
- };
+const browserConfig = {
+  input: "./es/lib/msRest.js",
+  external: [],
+  output: {
+    file: "./dist/msRest.browser.js",
+    format: "umd",
+    name: "msRest",
+    sourcemap: true,
+    banner
+  },
+  plugins: [
+    alias({
+      "./defaultHttpClient": "./defaultHttpClient.browser",
+      "./policies/msRestUserAgentPolicy": "./policies/msRestUserAgentPolicy.browser",
+      "./policies/proxyPolicy": "./policies/proxyPolicy.browser",
+      "./util/xml": "./util/xml.browser",
+      "./util/base64": "./util/base64.browser",
+    }),
+    nodeResolve({
+      module: true,
+      browser: true
+    }),
+    commonjs(),
+    sourcemaps(),
+    visualizer({
+      filename: "dist/browser-stats.html",
+      sourcemap: true
+    })
+  ]
+};
 
- export default [nodeConfig, browserConfig];
+export default [nodeConfig, browserConfig];
