7d84d10ab4f449778bbba8d7d4cddc77d62da8b9
Sarangan Rajamanickam <sarajama@microsoft.com>
2020-04-27 17:18:16 -0700

    Update lib/serviceClient.ts
    Co-Authored-By: Ramya Rao <ramya.rao.a@outlook.com>

--- lib/serviceClient.ts
+++ lib/serviceClient.ts
@@ -257,7 +257,7 @@ export class ServiceClient {
                 queryParameterValue = encodeURIComponent(queryParameterValue);
               }
             }
-            if (queryParameter.collectionFormat !== QueryCollectionFormat.Multi) {
+            if (queryParameter.collectionFormat != undefined && queryParameter.collectionFormat !== QueryCollectionFormat.Multi) {
               queryParameterValue = queryParameterValue.join(queryParameter.collectionFormat);
             }
             requestUrl.setQueryParameter(queryParameter.mapper.serializedName || getPathStringFromParameter(queryParameter), queryParameterValue);
