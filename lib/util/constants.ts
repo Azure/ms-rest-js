df388de146413639101713d2976dcd6bdd9acf5f
Jose Manuel Heredia Hidalgo <joheredi@microsoft.com>
2020-04-27 18:11:32 -0700

    Test disabling lock

--- lib/util/constants.ts
+++ lib/util/constants.ts
@@ -7,7 +7,7 @@ export const Constants = {
    * @const
    * @type {string}
    */
-  msRestVersion: "2.0.7",
+  msRestVersion: "2.0.8",
 
   /**
    * Specifies HTTP.
@@ -55,12 +55,12 @@ export const Constants = {
       POST: "POST",
       MERGE: "MERGE",
       HEAD: "HEAD",
-      PATCH: "PATCH"
+      PATCH: "PATCH",
     },
 
     StatusCodes: {
-      TooManyRequests: 429
-    }
+      TooManyRequests: 429,
+    },
   },
 
   /**
@@ -93,6 +93,6 @@ export const Constants = {
      * @const
      * @type {string}
      */
-    USER_AGENT: "User-Agent"
-  }
+    USER_AGENT: "User-Agent",
+  },
 };
