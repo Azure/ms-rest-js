c00f5cb0ca34a30cd68136a971de214ed0f452bf
Jeremy Meng <jeremy.ymeng@gmail.com>
2021-01-27 21:00:23 +0000

    Update instructions to obtain access token

--- samples/node-sample.ts
+++ samples/node-sample.ts
@@ -6,14 +6,11 @@ const clientOptions: msRest.ServiceClientOptions = {
 };
 
 const subscriptionId = process.env["AZURE_SUBSCRIPTION_ID"] || "subscriptionId";
-// An easy way to get the token
-// 1. Go to this test drive link https://azure.github.io/projects/apis and authenticate by clicking on Authorize. Check the user impersoantion checkbox in the popup.
-// 1.1 select a subscription of your choice
-// 1.2 select the storage-2015-06-15 option from the first drop down list
-// 1.3 expand the url to list storage accounts in a subscription
-// 1.4 click on try it out button.
-// 1.5 in the curl tab you will see the actual curl request that has the bearer token in it
-// 1.6 copy paste that token here. That token is valid for 1 hour
+// An easy way to get the token using Azure CLI (https://docs.microsoft.com/cli/azure/?view=azure-cli-latest)
+// 1. `az login` using the above subscription
+// 2. `az account set -s <subscription id>`
+// 3. `az account get-access-token --resource=https://management.azure.com`
+// 4. copy paste that token here. That token is valid for 1 hour
 const token = process.env["ACCESS_TOKEN"] || "token";
 const creds = new msRest.TokenCredentials(token);
 const client = new msRest.ServiceClient(creds, clientOptions);
