bf4c78d64268fe9bbcdf40e6c858193c1dbcfa90
Matthew Rathbone <matthew.rathbone@gmail.com>
2021-05-04 16:04:32 -0500

    added xmldom to remove DOM dependency - works in node and webworkers now

--- lib/util/xml.browser.ts
+++ lib/util/xml.browser.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
+import { DOMParser, XMLSerializer } from 'xmldom'
 
 const parser = new DOMParser();
 export function parseXML(str: string): Promise<any> {
